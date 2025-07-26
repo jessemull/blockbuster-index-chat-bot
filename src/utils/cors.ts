db810ef797ffda9cd233785c67bf3cdf5f326847
Jesse Mull <jessemull@Jesses-Laptop.local>
2025-07-25 22:32:25 -0700

    fix: fix-cors-again

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -12,7 +12,6 @@ export function getCorsHeaders(origin?: string) {
     "Access-Control-Allow-Headers":
       "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
     "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-    "Access-Control-Allow-Credentials": "true",
     "Content-Type": "application/json",
   };
 }
